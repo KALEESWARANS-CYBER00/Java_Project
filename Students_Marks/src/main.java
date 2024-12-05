bfe02fdd00e5d4d9b28c36fa593189deb984d0a7
KALEESWARANS-CYBER00 <skaleeswaran00@gmail.com>
2024-12-05 10:53:35 +0530

    Students Marks Calculation Code

--- Students_Marks/src/main.java
+++ Students_Marks/src/main.java
@@ -1,4 +1,8 @@
-/* author =@KALI */
+/* author =@KALI 
+ * About The Program :
+ * The main.java is main class for Deploy a code  
+*/
+
 public class main extends Mark_Calculation{
 
     public static void main(String[] args) {
