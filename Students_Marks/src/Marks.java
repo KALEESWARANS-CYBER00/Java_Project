578d0953cb6f16cf91fbcdbd3e33f1435ec9e4ea
KALEESWARANS-CYBER00 <skaleeswaran00@gmail.com>
2024-12-05 10:27:40 +0530

    Declare Or Initialize a Data

---
+++ Students_Marks/src/Marks.java
@@ -0,0 +1,12 @@
+/* author =@KALI */
+import java.util.Scanner;
+
+public class Marks {
+    public static int Mark[] = new int[5];
+    public static int total;
+    public static float avg;
+    public static String subject[] = { "🖉_Tamil", "🖊_English", "🖋_Maths", "🖌_science", "🖍_social" };
+    public static Boolean PF = false;
+    public static String Line="--------------------------------------------------------------------------------------";
+    static Scanner mark = new Scanner(System.in);
+}
