2b36bf04f5c2955e1650573c308abc16f880c86b
KALEESWARANS-CYBER00 <skaleeswaran00@gmail.com>
2024-12-05 10:45:20 +0530

    Get a Data From All Parents Class Through Inheritence

---
+++ Students_Marks/src/Mark_Calculation.java
@@ -0,0 +1,21 @@
+/* author =@KALI */
+
+public class Mark_Calculation extends Rank {
+
+    static void Mark_Calculationf() {
+        System.out.println(Line);
+        Set_Marks();
+        System.out.println(Line);
+        Set_Total_Marks();
+        System.out.println(Line);
+        Get_Marks();
+        System.out.println(Line);
+        Get_AvgTot();
+        System.out.println(Line);
+        Check_PF();
+        System.out.println(Line);
+        Rankf();
+        System.out.println(Line);
+    }
+
+}
